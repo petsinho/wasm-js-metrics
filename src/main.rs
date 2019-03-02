1b2180051141cd0be5616577183d2082766bced4
petsinho <panagiotis.petsas@gmail.com>
2019-03-03 00:19:33 +0200

    added mouse movement event, formatted code

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,59 @@
 #[macro_use]
 extern crate stdweb;
+extern crate time;
 
 use std::rc::Rc;
 
+use time::{Timespec, Tm};
+
 use stdweb::traits::*;
 use stdweb::unstable::TryInto;
-use stdweb::web::{
-    HtmlElement,
-    document,
-};
+use stdweb::web::{document, HtmlElement};
 
-use stdweb::web::event::{
-    KeyPressEvent,
-    ClickEvent,
-};
+use stdweb::web::event::{ClickEvent, KeyPressEvent, MouseMoveEvent};
 
 use stdweb::web::html_element::InputElement;
 
 mod utils;
 
+pub struct EventInfo {
+    pub element_name: String,
+    pub event_type: String,
+    created_at: Option<i64>,
+    completed_at: Option<i64>,
+}
+
+pub struct Coords {
+    pub x: u32,
+    pub y: u32,
+}
+
+impl EventInfo {
+    fn created_at(&self) -> Option<Tm> {
+        match self.created_at {
+            None => None,
+            Some(t) => Some(time::at(Timespec::new(t, 0))),
+        }
+    }
+
+    fn completed_at(&self) -> Option<Tm> {
+        match self.completed_at {
+            None => None,
+            Some(t) => Some(time::at(Timespec::new(t, 0))),
+        }
+    }
+}
+
 fn main() {
     println!("started...");
     stdweb::initialize();
 
-    let output_div: HtmlElement = document().query_selector( ".output" ).unwrap().unwrap().try_into().unwrap();
+    let output_div: HtmlElement = document()
+        .query_selector(".output")
+        .unwrap()
+        .unwrap()
+        .try_into()
+        .unwrap();
     let output_msg = Rc::new(move |msg: &str| {
         let elem = document().create_element("p").unwrap();
         elem.set_text_content(msg);
@@ -35,24 +65,40 @@ fn main() {
     });
     output_msg("> Starting...");
 
-
-
-  let body: HtmlElement = document().query_selector( ".bod" ).unwrap().unwrap().try_into().unwrap();
-    body.add_event_listener( enclose!( () move |event: ClickEvent| {
+    let body: HtmlElement = document()
+        .query_selector(".bod")
+        .unwrap()
+        .unwrap()
+        .try_into()
+        .unwrap();
+    body.add_event_listener(enclose!( () move |event: ClickEvent| {
         let target = event.target().unwrap();
         output_msg("clicked body ");
         js!{
             console.log("target", event.target)
         }
         // if event.key() == "Enter" {
-        //     //TODO: save event            
+        //     //TODO: save event
         // }
     }));
 
-    let text_entry: InputElement = document().query_selector( ".form input" ).unwrap().unwrap().try_into().unwrap();
-    text_entry.add_event_listener( enclose!( (text_entry) move |event: KeyPressEvent| {
+    body.add_event_listener(enclose!( () move |event: MouseMoveEvent| {
+        let posx = event.client_x();
+        let posy = event.client_y();
+
+        js!{
+            console.log("mouse moved @ {}, {} ", @{posx}, @{posy})
+        }
+    }));
+
+    let text_entry: InputElement = document()
+        .query_selector(".form input")
+        .unwrap()
+        .unwrap()
+        .try_into()
+        .unwrap();
+    text_entry.add_event_listener(enclose!( (text_entry) move |event: KeyPressEvent| {
         if event.key() == "Enter" {
-            println!("enter bitch");
             event.prevent_default();
 
             let text: String = text_entry.raw_value();
