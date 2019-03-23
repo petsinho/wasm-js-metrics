83e4197e3edf5354c1279a79160a788c17d017bf
petsinho <panagiotis.petsas@gmail.com>
2019-03-23 20:58:09 +0200

    capturing click event with element info and date

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
 #[macro_use]
 extern crate stdweb;
-extern crate time;
 
+#[allow(unused_imports)]
+use std::time::{SystemTime, UNIX_EPOCH};
 use std::rc::Rc;
 
-use time::{Timespec, Tm};
 
 use stdweb::traits::*;
 use stdweb::unstable::TryInto;
 use stdweb::web::{document, HtmlElement};
-
+use stdweb::web::Date;
 use stdweb::web::event::{ClickEvent, KeyPressEvent, MouseMoveEvent};
 
 use stdweb::web::html_element::InputElement;
-
 mod utils;
 
 pub struct EventInfo {
     pub element_name: String,
+    pub element_id: String,
     pub event_type: String,
-    created_at: Option<i64>,
-    completed_at: Option<i64>,
+    pub created_at: f64 ,
+    // completed_at: Option<SystemTime>,
 }
 
 pub struct Coords {
@@ -28,21 +28,21 @@ pub struct Coords {
     pub y: u32,
 }
 
-impl EventInfo {
-    fn created_at(&self) -> Option<Tm> {
-        match self.created_at {
-            None => None,
-            Some(t) => Some(time::at(Timespec::new(t, 0))),
-        }
-    }
+// impl EventInfo {
+//     fn created_at(&self) -> Option<Tm> {
+//         match self.created_at {
+//             None => None,
+//             Some(t) => Some(time::at(Timespec::new(t, 0))),
+//         }
+//     }
 
-    fn completed_at(&self) -> Option<Tm> {
-        match self.completed_at {
-            None => None,
-            Some(t) => Some(time::at(Timespec::new(t, 0))),
-        }
-    }
-}
+//     fn completed_at(&self) -> Option<Tm> {
+//         match self.completed_at {
+//             None => None,
+//             Some(t) => Some(time::at(Timespec::new(t, 0))),
+//         }
+//     }
+// }
 
 fn main() {
     println!("started...");
@@ -73,13 +73,27 @@ fn main() {
         .unwrap();
     body.add_event_listener(enclose!( () move |event: ClickEvent| {
         let target = event.target().unwrap();
+        // println!("{:?} target",  target);
+
+        let el_name: String = js!(return @{target.as_ref()}.localName).try_into().unwrap();
+        let el_id: String = js!(return @{target.as_ref()}.id).try_into().unwrap();
+        
+        let event_info = EventInfo {
+            element_name :  el_name.clone(),
+            element_id :  el_id.clone(),
+            event_type : "click".to_string(),
+            created_at:   Date::now(),
+            // completed_at: None,
+
+        };
         output_msg("clicked body ");
+        output_msg(&el_name);
         js!{
-            console.log("target", event.target)
+            console.log("target element nodename-->",
+             @{event}.toElement.nodeName
+             )
         }
-        // if event.key() == "Enter" {
-        //     //TODO: save event
-        // }
+
     }));
 
     body.add_event_listener(enclose!( () move |event: MouseMoveEvent| {
